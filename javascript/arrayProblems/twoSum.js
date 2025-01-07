7c8d6dbda0c4c87720999e3b4ab82a1c13b444b6
Sahil Chopra <sahilchopra838@gmail.com>
2025-01-08 00:00:31 +0530

    array two sum problem

--- javascript/arrayProblems/twoSum.js
+++ javascript/arrayProblems/twoSum.js
@@ -1,23 +1,16 @@
 var twoSum = function(nums, target) {
-
     const hash = {};
     let n = nums.length;
 
-    for(let i = 0; i <= n -1; i ++){
-        hash[nums[i]] = i;
+    for (let i = 0; i < n; i++) {
+        const complement = target - nums[i];
 
-    }
-    for(let i = 0; i <= n -1; i ++){
-        hash[nums[i]] = target - nums[i];
-        if(hash[target-nums[i]]){
-            return [i, hash[target-nums[i]]]
+        if (complement in hash) {
+            return [hash[complement], i];
         }
 
-      
-
+        hash[nums[i]] = i;
     }
+
     return [];
-
 };
-
-console.log(twoSum([3,4,9,6,4], 8))
